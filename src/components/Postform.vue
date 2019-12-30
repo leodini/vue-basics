4e3b8eb4f16b4d35c98e4d27cba0121175e3b498
leodini <leodini@hotmail.com>
2019-12-30 02:37:26 -0300

    edit and delete methods

--- src/components/Postform.vue
+++ src/components/Postform.vue
@@ -3,15 +3,17 @@
         <form v-if="!loading" class="form" @submit.prevent="onSubmit">
             <div class="input-field">
                 <label for="title">Title</label>
-                <input type="text" name="title" v-model="title" class="validate">
+                <input type="text" name="title" v-model="title" :class="[errors.title ? 'invalid' : 'validate']">
                 <span class="helper-text" data-error="Title must not be empty"></span>
             </div>
             <div class="input-field">
                 <label for="body">Body</label>
-                <input type="text" name="body" v-model="body" class="validate">
+                <input type="text" name="body" v-model="body" :class="[errors.body ? 'invalid' : 'validate']">
                 <span class="helper-text" data-error="Body must not be empty"></span>
             </div>
-            <button type="submit" class="waves-effect waves-light btn">Add</button>
+            <button type="submit" class="waves-effect waves-light btn">
+                {{ id ? 'Update' : 'Add' }}
+            </button>
         </form>
         <div class="progress" v-else-if="loading">
             <div class="indeterminate"></div>
@@ -24,34 +26,68 @@ import PostService from '../PostService'
 const postService = new PostService()
 export default {
     name: 'Postform',
+    props: {
+        editingPost: Object
+    },
     data(){
         return{
             loading: false,
             title: '',
-            body: ''
+            body: '',
+            errors: {},
+            id: null
         }
     },
     methods: {
         onSubmit(){
             this.loading = true;
+            if(!this.validForm()){
+                this.loading = false;
+                return
+            }
             const post = {
                 title: this.title,
-                body: this.body
+                body: this.body,
+                id: this.id
             }
             postService.writePost(post)
             .then(res => {
                 this.loading = false
-                this.body = '',
+                this.body = ''
                 this.title = ''
+                this.$emit('postCreated', res.data)
             })
-            .catch(err => console.log(err))
+            .catch(err => alert(err))
+        }
+    },
+        validForm(){
+            this.errors = {}
+            if(this.title.trim() === ''){
+                this.errors.title = 'Title'
+            }
+            if(this.body.trim() === ''){
+                this.errors.body = 'Body'
+            }
+            if(Object.keys(this.errors).length > 0){
+                return false;
+            } else return true
+    },
+    watch:{
+        editingPost(post){
+            this.title = post.title
+            this.body = post.body
+            this.id = post.id
         }
     }
+    
 }
 </script>
 
 <style scoped>
     .form{
         margin: 50px;
+    }
+    .progress{
+        margin: 100px 0;
     }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,12 @@
     <div class="row">
         <div class="col s6">
             <!-- Form -->
-            <Postform/>
+            <Postform @postCreated="addPost" :editingPost="editingPost"/>
+        </div>
+        <div class="col s3">
+            <p>Limit number of posts</p>
+            <input type="number" v-model="postLimit">
+            <button @click="setLimit()" class="waves-effect waves-light btn">Set</button>
         </div>
     </div>
     <div class="row">
@@ -15,12 +20,12 @@
             <div class="card">
                 <div class="card-content">
                     <p class="card-title">{{ post.title }}</p>
-                    <p class="timestamp">{{ post.createdAt }}</p>
+                    <p class="timestamp">{{ post.createdAt | formatDate }}</p>
                     <p>{{ post.body }}</p>
                 </div>
                 <div class="card-action">
-                    <a href="#">Edit</a>
-                    <a href="#" class="delete-btn">Delete</a>
+                    <a href="#" @click="editPost(post)">Edit</a>
+                    <a href="#" class="delete-btn" @click="deletePost(post.id)">Delete</a>
                 </div>
             </div>
         </div>
@@ -39,16 +44,51 @@ export default {
     },
     data(){
         return{
-            posts: []
+            posts: [],
+            postLimit: 5,
+            editingPost: null
+        }
+    },
+    methods: {
+        addPost(post){
+            if(this.posts.find(p => p.id === post.id)){
+                const index = this.posts.findIndex(p => p.id === post.id)
+                this.posts.splic(index, 1, post)
+            } else {
+                this.posts.unshift(post)
+            }
+        },
+        editPost(post){
+            this.editingPost = post
+        },
+        deletePost(id){
+            postService.deletePost(id)
+            .then(() => {
+                this.posts = this.posts.filter(p => p.id !== id)
+            })
+            .catch(err => alert(err))
+        },
+        setLimit(){
+            postService.getPost(this.postLimit)
+            .then(res => this.posts = res.data)
+            .catch(err => alert(err))
         }
     },
     created(){
         postService.getAllPosts()
         .then(res => {
-            this.posts = res.data
-            // console.log(this.posts)
+            this.posts = res.data    
         })
         .catch(err => alert(err))
+    },
+    filters: {
+        formatDate(date){
+            date = new Date(date)
+            const day = date.getDate()
+            const month = date.getMonth() + 1
+            const year = date.getFullYear()
+            return `${day}-${month}-${year}`
+        }
     }
 } 
 
